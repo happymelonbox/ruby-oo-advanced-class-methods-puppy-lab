1cf258b6a24968b7ff0972cf098edc26f5a496a8
Justin Berger Howes <howes.j.j@gmail.com>
2021-08-30 09:03:55 +1000

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Add your code here
+require 'pry'
+class Dog
+    @@all = []
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        puts @@all.collect{|dog| dog.name}
+    end
+
+end
